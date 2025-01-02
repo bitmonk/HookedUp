34f4bbc7ecaabb6e02c4c33fcddd090c65b40e7e
Parbat Tamang <parbattamang1234@gmail.com>
2025-01-02 16:17:13 +0545

    minor changes

--- lib/screen/chat/chat_page.dart
+++ lib/screen/chat/chat_page.dart
@@ -13,77 +13,83 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.backgroundColor,
-      appBar: AppBar(
-        surfaceTintColor: AppColors.backgroundColor,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 25.h,
-            width: 25.w,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Color(0xFF9FA482).withAlpha(42),
-            ),
-            child: Icon(
-              Icons.arrow_back,
-              size: 19,
-            ),
-          ),
-        ),
-        title: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: Image.asset(
-                'assets/images/explainer/profile.png',
-                width: 40,
-                height: 40,
-                fit: BoxFit.cover,
+        backgroundColor: AppColors.backgroundColor,
+        appBar: AppBar(
+          surfaceTintColor: AppColors.backgroundColor,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 25.h,
+              width: 25.w,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color(0xFF9FA482).withAlpha(42),
+              ),
+              child: Icon(
+                Icons.arrow_back,
+                size: 19,
               ),
             ),
-            SizedBox(width: 12),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Sarah Smith',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
-                  ),
+          ),
+          title: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.asset(
+                  'assets/images/explainer/profile.png',
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
                 ),
-                Text(
-                  'Online',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.green,
+              ),
+              SizedBox(width: 12),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sarah Smith',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-              ],
+                  Text(
+                    'Online',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF212221).withAlpha(60),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.more_horiz,
+                color: Color(0xFFD9D9D9),
+                size: 30,
+              ),
+              onPressed: () {
+                print('More options tapped');
+              },
             ),
           ],
         ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.more_horiz,
-              color: Color(0xFFD9D9D9),
-              size: 30,
-            ),
-            onPressed: () {
-              print('More options tapped');
-            },
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Divider(
+                height: 2,
+                color: Color(0xFFF5F5F5),
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Center(
-        child: Text('data'),
-      ),
-    );
+        ));
   }
 }
