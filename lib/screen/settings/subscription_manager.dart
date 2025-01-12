d9354a98782be4d5bd087870d0b5107c96b2dc49
Parbat Tamang <parbattamang1234@gmail.com>
2025-01-12 21:00:20 +0545

    sub manager (settings) ui minor changes

--- lib/screen/settings/subscription_manager.dart
+++ lib/screen/settings/subscription_manager.dart
@@ -21,41 +21,36 @@ class _SubscriptionManagerState extends State<SubscriptionManager> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(255, 255, 252, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Padding(
+          padding: EdgeInsets.only(left: 12.0),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              width: 10.0,
+              height: 10.0,
+              decoration: BoxDecoration(
+                color: const Color(0xFF9FA482).withAlpha(42),
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
+                Icons.arrow_back,
+                color: Color(0xFF2B361C),
+              ),
+            ),
+          ),
+        ),
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.w),
             child: Column(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 20.h),
-                  child: AppBar(
-                    backgroundColor: Colors.transparent,
-                    elevation: 0,
-                    leading: GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color:
-                              const Color(0xFF9FA482).withValues(alpha: 0.42),
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Icon(
-                          Icons.arrow_back,
-                          color: Color(0xFF2B361C),
-                        ),
-                      ),
-                    ),
-                    title: SvgPicture.asset(
-                      'assets/images/splash/logo.svg',
-                      height: 54.h,
-                      width: 52.w,
-                    ),
-                    centerTitle: true,
-                  ),
-                ),
                 SizedBox(height: 31.h),
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 14.w),
@@ -207,17 +202,17 @@ class _SubscriptionManagerState extends State<SubscriptionManager> {
                       Stack(
                         children: [
                           TextField(
+                            readOnly: true,
                             decoration: InputDecoration(
+                              hintText: 'Mary Jackson',
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(20.r),
                                 borderSide: BorderSide.none,
                               ),
                               filled: true,
                               fillColor: const Color(0xFFF5F5F5),
-                              contentPadding: EdgeInsets.symmetric(
-                                horizontal: 24.w,
-                                vertical: 18.h,
-                              ),
+                              contentPadding: EdgeInsets.only(
+                                  top: 24.h, left: 24.w, bottom: 12.h),
                             ),
                           ),
                           Positioned(
@@ -239,18 +234,18 @@ class _SubscriptionManagerState extends State<SubscriptionManager> {
                       Stack(
                         children: [
                           TextField(
+                            readOnly: true,
                             keyboardType: TextInputType.number,
                             decoration: InputDecoration(
+                              hintText: '**** **** **** 1234',
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(20.r),
                                 borderSide: BorderSide.none,
                               ),
                               filled: true,
                               fillColor: const Color(0xFFF5F5F5),
-                              contentPadding: EdgeInsets.symmetric(
-                                horizontal: 24.w,
-                                vertical: 18.h,
-                              ),
+                              contentPadding: EdgeInsets.only(
+                                  top: 24.h, left: 24.w, bottom: 12.h),
                             ),
                           ),
                           Positioned(
